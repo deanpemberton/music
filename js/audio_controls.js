894c81432a9fcd2d7bbc11570328dad3e0bf1e86
Andy Linton <asjl@lpnz.org>
2016-12-05 11:56:30 +1300

    Added ‘ID’ to ‘button’ variables for readability

--- js/audio_controls.js
+++ js/audio_controls.js
@@ -14,61 +14,58 @@ function setPlaySpeed(speed, audioID) {
 var BeginLoopTime = 0;
 var EndLoopTime = 0;
 var PreviousAudioID = null;
-var PreviousButton1 = null;
-var PreviousButton2 = null;
+var PreviousButton1ID = null;
+var PreviousButton2ID = null;
 
-function SetPlayRange(audioID, ButtonEvent, button1, button2) {
+function SetPlayRange(audioID, ButtonEvent, button1ID, button2ID) {
     if(audioID.readyState == 0) {
         return;
     }
     if(PreviousAudioID != audioID) {
         BeginLoopTime = 0;
         EndLoopTime = 0;
-        if(PreviousButton1 != null) {
+        if(PreviousButton1ID != null) {
             var zero = 0;
-            PreviousButton1.value = zero.toFixed(1);
-            PreviousButton2.value = PreviousAudioID.duration.toFixed(1);
+            PreviousButton1ID.value = zero.toFixed(1);
+            PreviousButton2ID.value = PreviousAudioID.duration.toFixed(1);
             // Consider deregistering the function here
         }    
         PreviousAudioID = audioID;
-        PreviousButton1 = button1;
-        PreviousButton2 = button2;
+        PreviousButton1ID = button1ID;
+        PreviousButton2ID = button2ID;
     }
     if (EndLoopTime == 0) {
         EndLoopTime = audioID.duration - .25; // sampling rate is ~ every 250 ms (overshoot the end)
     }
+    /*
+     * Set the start and end of loop markers depending on which button was pressed
+     */
     switch (ButtonEvent) {
+        // Loop Start button
         case 0:
             BeginLoopTime = audioID.currentTime;
-            button1.value = BeginLoopTime.toFixed(1);
-            button2.value = EndLoopTime.toFixed(1);
-            audioID.addEventListener("timeupdate", function() {
-                if (audioID.currentTime >= EndLoopTime) {
-                    audioID.currentTime = BeginLoopTime;
-                }}, false);
-            return;
+            button1ID.value = BeginLoopTime.toFixed(1);
+            button2ID.value = EndLoopTime.toFixed(1);
             break;
+        // Loop End button
         case 1:
             EndLoopTime = audioID.currentTime;
-            button1.value = BeginLoopTime.toFixed(1);
-            button2.value = EndLoopTime.toFixed(1);
-            audioID.addEventListener("timeupdate", function() {
-                if (audioID.currentTime >= EndLoopTime) {
-                    audioID.currentTime = BeginLoopTime;
-                }}, false);
-            return;
+            button1ID.value = BeginLoopTime.toFixed(1);
+            button2ID.value = EndLoopTime.toFixed(1);
             break;
-        case 3:
+        // Reset button
+        case 2:
             BeginLoopTime = 0;
             EndLoopTime = audioID.duration - .25;
-            button1.value = BeginLoopTime.toFixed(1);
-            button2.value = EndLoopTime.toFixed(1);
-            audioID.addEventListener("timeupdate", function() {
-                if (audioID.currentTime >= EndLoopTime) {
-                    audioID.currentTime = BeginLoopTime;
-                }}, false);
-            return;
+            button1ID.value = BeginLoopTime.toFixed(1);
+            button2ID.value = EndLoopTime.toFixed(1);
             break;    
     }
+
+    audioID.addEventListener("timeupdate", function() {
+        if (audioID.currentTime >= EndLoopTime) {
+            audioID.currentTime = BeginLoopTime;
+        }}, false);
+    return;
 }
 
